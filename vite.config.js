a2d5f2395f5dba5055e5b0387676117f9aa0ae29
dheerajgupta2006 <162675205+dheerajgupta2006@users.noreply.github.com>
2025-11-06 12:11:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:2069
-  }
+  base: '/ecommerce/',  // 👈 Added: ensures all assets load under /ecommerce/
+  server: {
+    port: 2069,
+  },
 })
